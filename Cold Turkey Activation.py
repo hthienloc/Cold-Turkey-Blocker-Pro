8862d2a5652bed507b2f5cd0aa40f3b2ae0fb82e
hthienloc <148019203+hthienloc@users.noreply.github.com>
2023-10-29 20:49:58 +0700

    Update Cold Turkey Activation.py

--- Cold Turkey Activation.py
+++ Cold Turkey Activation.py
@@ -10,23 +10,19 @@ def activate():
     try:
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
-        s = c.execute(
-            "SELECT value FROM settings WHERE key = 'settings'").fetchone()[0]
+        s = c.execute("SELECT value FROM settings WHERE key = 'settings'").fetchone()[0]
         dat = json.loads(s)
         if dat["additional"]["proStatus"] != "pro":
             print("Your version of Cold Turkey Blocker is not activated.")
             dat["additional"]["proStatus"] = "pro"
-            print(
-                "But now it is activated.\nPlease close Cold Turkey Blocker and run it again.")
-            c.execute(
-                """UPDATE settings SET value = ? WHERE "key" = 'settings'""", (json.dumps(dat),))
+            print("But now it is activated.\nPlease close Cold Turkey Blocker and run it again.")
+            c.execute("""UPDATE settings SET value = ? WHERE "key" = 'settings'""", (json.dumps(dat),))
             conn.commit()
         else:
             print("Looks like your copy of Cold Turkey Blocker is already activated.")
             print("Deactivating it now.")
             dat["additional"]["proStatus"] = "free"
-            c.execute(
-                "UPDATE settings set value =  WHERE key = 'settings'", (json.dumps(dat),))
+            c.execute("UPDATE settings set value =  WHERE key = 'settings'", (json.dumps(dat),))
             conn.commit()
     except sqlite3.Error as e:
         print("Failed to activate", e)
